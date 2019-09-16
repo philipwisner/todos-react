94d25bdffe6e8391e9060df4b7520e7e46661e34
Philip Wisner <philipwisner@gmail.com>
2019-09-16 15:57:20 -0400

    created basic app and nav

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from "./pages/Home";
+import About from "./pages/About";
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>TEST</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="NavHeader">
+        <div href="/" className="NavChildTitle">
+          <a href="/">Todos</a>
+        </div>
+        <div className="NavChildInfo">
+          <a href="/about">Info</a>
+        </div>
+      </h1>
+      <Router>
+        <div>
+          <Switch>
+            <Route path="/about" component={About} />
+            <Route path="/" component={Home} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
