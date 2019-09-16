94df61e9fc883b981e624f43819f1519e15ab498
Philip Wisner <philipwisner@gmail.com>
2019-09-16 15:56:48 -0400

    created page containers

---
+++ src/containers/AboutContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/Global.scss";
+import "../styles/Containers.scss";
+
+const AboutContainer = () => {
+  return (
+    <div className="Container">
+      <h1 className="PrimaryHeader">About</h1>
+    </div>
+  );
+};
+
+export default AboutContainer;
---
+++ src/containers/TodoListContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/Global.scss";
+import "../styles/Containers.scss";
+
+const TodoListContainer = () => {
+  return (
+    <div className="Container">
+      <p>Todo list here</p>
+    </div>
+  );
+};
+
+export default TodoListContainer;
