9e069b1cc01f9b662c844613dc11e4e52b5ea05f
Philip Wisner <philipwisner@gmail.com>
2019-09-17 10:40:50 -0400

    added demo data

---
+++ src/data.js
@@ -0,0 +1,36 @@
+const appData = {};
+
+appData.todos = [
+  {
+    id: 1,
+    content: "Grocery Shopping",
+    completed: true
+  },
+  {
+    id: 2,
+    content: "Clean Room",
+    completed: false
+  },
+  {
+    id: 3,
+    content: "Study Spanish",
+    completed: false
+  },
+  {
+    id: 4,
+    content: "Learn Git",
+    completed: false
+  },
+  {
+    id: 5,
+    content: "Learn React",
+    completed: false
+  },
+  {
+    id: 6,
+    content: "Learn Redux",
+    completed: false
+  }
+];
+
+module.exports = appData;
