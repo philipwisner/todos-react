508cee99371ea6c76e77cff1ea49543cbb6746a0
Philip Wisner <philipwisner@gmail.com>
2019-09-18 00:04:28 -0400

    passed toggle down

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,17 +1,13 @@
-import React from "react";
-import { Todo } from "./";
-import "../styles/ToDoList.scss";
+import React from 'react';
+import { Todo } from './';
+import '../styles/ToDoList.scss';
 
 //This needs props of todos so that it can generate todo list based off filter status
-
-const TodoList = () => {
-  return (
-    <div className="ToDoList">
-      <Todo />
-      <Todo />
-      <Todo />
-    </div>
-  );
+const TodoList = props => {
+  const todoList = props.todos.map(todo => {
+    return <Todo todo={todo} key={todo.id} toggleTodo={props.toggleTodo} />;
+  });
+  return <div className="ToDoList">{todoList}</div>;
 };
 
 export default TodoList;
