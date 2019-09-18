fb2d50befb9b54019d4486c88fb0d8d86ef395f5
Philip Wisner <philipwisner@gmail.com>
2019-09-18 00:02:55 -0400

    added button function

--- src/components/BottomButton.js
+++ src/components/BottomButton.js
@@ -1,13 +1,13 @@
-import React from "react";
-import "../styles/ToDoList.scss";
+import React from 'react';
+import '../styles/ToDoList.scss';
 
-//When clicked it should make all visible items completed
-//This should be dynamic and change based off filter option
-
-const BottomButton = () => {
+const BottomButton = props => {
   return (
     <div className="BottomButton">
-      <button className="Button">Mark All Completed</button>
+      <button className="Button" onClick={props.toggleAllTodos}>
+        Mark All
+        <span> {props.checkAll ? 'Completed' : 'Pending'}</span>
+      </button>
     </div>
   );
 };
