7a3bb481c3e55f8764699f21df54f371128a451c
Philip Wisner <philipwisner@gmail.com>
2019-09-18 00:05:05 -0400

    cleanup

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import '../styles/ToDoList.scss';
 
-//This needs to recieve props from parent to display todo with id
-//This needs to send action to parent that item's status was changed with id
-
 class Todo extends React.Component {
   render() {
     const { todo, toggleTodo } = this.props;
