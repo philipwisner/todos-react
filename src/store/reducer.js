75e3200a42b5cf3bcb0cbaa261c6fe34e0a840a0
Philip Wisner <philipwisner@gmail.com>
2019-09-17 22:53:15 -0400

    updated reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,32 +1,45 @@
-import ACTIONS from "./action";
+import { combineReducers } from "redux";
+import { addTodo, toggleTodo, setFilter, FilterTypes } from "./action";
 import _ from "lodash";
+import appData from "../data";
+const { SHOW_ALL, SHOW_PENDING, SHOW_COMPLETED } = FilterTypes;
 
-const defaultState = {
-  items: []
-};
-
-const todoReducer = (state = defaultState, action) => {
+function visibilityFilter(state = SHOW_ALL, action) {
   switch (action.type) {
-    case ACTIONS.Types.CREATE_ITEM: {
-      console.log(action);
-
-      let item = action.payload;
-      let newItem = { id: state.items.length + 1, description: item };
-      let newState = _.cloneDeep(state);
-      newState.items.push(newItem);
-      return newState;
-    }
-
-    case ACTIONS.Types.DELETE_ITEM: {
-      let newState = _.cloneDeep(state);
-      let index = _.findIndex(newState.items, { id: action.payload });
-      newState.items.splice(index, 1);
-      return newState;
-    }
-
+    case setFilter:
+      return action.filter;
     default:
       return state;
   }
-};
+}
 
-export default todoReducer;
+function todos(state = appData.todos, action) {
+  switch (action.type) {
+    case addTodo:
+      return [
+        ...state,
+        {
+          text: action.text,
+          completed: false
+        }
+      ];
+    case toggleTodo:
+      return state.map((todo, index) => {
+        if (index === action.index) {
+          return Object.assign({}, todo, {
+            completed: !todo.completed
+          });
+        }
+        return todo;
+      });
+    default:
+      return state;
+  }
+}
+
+const todoApp = combineReducers({
+  visibilityFilter,
+  todos
+});
+
+export default todoApp;
