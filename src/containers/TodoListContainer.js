3baff512d16210c69440b279ada2e3d4dd4f4a1b
Philip Wisner <philipwisner@gmail.com>
2019-09-18 00:05:22 -0400

    added all functions and passed

--- src/containers/TodoListContainer.js
+++ src/containers/TodoListContainer.js
@@ -1,16 +1,63 @@
-import React from "react";
-import { NewTodo, FilterBar, TodoList, BottomButton } from "../components";
-import "../styles/Containers.scss";
+import React from 'react';
+import { NewTodo, FilterBar, TodoList, BottomButton } from '../components';
+import '../styles/Containers.scss';
+import appData from '../data';
 
-const TodoListContainer = () => {
-  return (
-    <div className="Home">
-      <NewTodo />
-      <FilterBar />
-      <TodoList />
-      <BottomButton />
-    </div>
-  );
-};
+class TodoListContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: appData.todos,
+      selectedFilter: 'all',
+      checkAll: true,
+    };
+  }
+  toggleFilter = value => {
+    this.setState({ selectedFilter: value });
+  };
+
+  addTodo = todo => {
+    const { todos } = this.state;
+    this.setState({ todos: [...todos, todo] });
+  };
+
+  toggleTodo = selectedTodo => {
+    const { todos } = this.state;
+    this.setState({
+      todos: todos.map(todo => {
+        todo.id == selectedTodo.id
+          ? (todo.completed = !todo.completed)
+          : (todo.completed = todo.completed);
+        return todo;
+      }),
+    });
+  };
+
+  toggleAllTodos = () => {
+    const { todos, checkAll } = this.state;
+    this.setState({
+      todos: todos.map(todo => {
+        this.state.checkAll ? (todo.completed = true) : (todo.completed = false);
+        return todo;
+      }),
+      checkAll: !checkAll,
+    });
+  };
+
+  render() {
+    return (
+      <div className="Home">
+        <NewTodo addTodo={this.addTodo} />
+        <FilterBar selectedFilter={this.state.selectedFilter} toggleFilter={this.toggleFilter} />
+        <TodoList
+          todos={this.state.todos}
+          selectedFilter={this.state.selectedFilter}
+          toggleTodo={this.toggleTodo}
+        />
+        <BottomButton checkAll={this.state.checkAll} toggleAllTodos={this.toggleAllTodos} />
+      </div>
+    );
+  }
+}
 
 export default TodoListContainer;
