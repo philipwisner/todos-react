b04b68c9c3b5061014b1fbc97bfd11d4be503767
Philip Wisner <philipwisner@gmail.com>
2019-09-18 00:04:45 -0400

    added toggle todo function

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,17 +1,23 @@
-import React from "react";
-import "../styles/ToDoList.scss";
+import React from 'react';
+import '../styles/ToDoList.scss';
 
 //This needs to recieve props from parent to display todo with id
 //This needs to send action to parent that item's status was changed with id
 
 class Todo extends React.Component {
-  state = {};
-
   render() {
+    const { todo, toggleTodo } = this.props;
     return (
       <div className="ToDoItem">
-        <input type="checkbox" id="1" />
-        <label htmlFor="1">Grocery Shopping</label>
+        <input
+          type="checkbox"
+          id={todo.id}
+          checked={todo.completed}
+          onChange={() => toggleTodo(todo)}
+        />
+        <label htmlFor={todo.id} className="CheckBoxText">
+          {todo.text}
+        </label>
       </div>
     );
   }
