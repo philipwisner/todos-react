9ccc03684627ff8aeebef5226989e8243ddd3975
Philip Wisner <philipwisner@gmail.com>
2019-09-20 09:39:43 -0400

    added color picker component

---
+++ src/components/ColorPicker.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../styles/Settings.scss';
+
+const ColorPicker = props => {
+  const { colors, selectedColor, setThemeColor } = props;
+
+  const colorTiles = colors.map(color => {
+    return (
+      <div
+        key={color.value}
+        className={selectedColor === color.value ? 'selected color-tile' : 'color-tile'}
+        style={{ backgroundColor: color.value }}
+        onClick={() => setThemeColor(color.value)}
+      ></div>
+    );
+  });
+  return <div className="color-tiles">{colorTiles}</div>;
+};
+
+export default ColorPicker;
