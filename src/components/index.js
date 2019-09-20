8934a1229fd3188344887255ae0dcb314e972c32
Philip Wisner <philipwisner@gmail.com>
2019-09-20 09:39:30 -0400

    updated index

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
-export { default as BottomButton } from "./BottomButton";
-export { default as FilterBar } from "./FilterBar";
-export { default as HeaderNav } from "./HeaderNav";
-export { default as NewTodo } from "./NewTodo";
-export { default as Todo } from "./Todo";
-export { default as TodoList } from "./TodoList";
+export { default as BottomButton } from './BottomButton';
+export { default as FilterBar } from './FilterBar';
+export { default as HeaderNav } from './HeaderNav';
+export { default as NewTodo } from './NewTodo';
+export { default as Todo } from './Todo';
+export { default as TodoList } from './TodoList';
+export { default as ColorPicker } from './ColorPicker';
