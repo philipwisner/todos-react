d1c46565d6e104c3923b4e0ed817bc8876cd86c1
Philip Wisner <philipwisner@gmail.com>
2019-09-20 09:39:38 -0400

    added colors and check

--- src/data.js
+++ src/data.js
@@ -39,4 +39,17 @@ appData.statuses = [
   { name: 'Completed', value: 'completed' },
 ];
 
+appData.colors = [
+  { name: 'Grey', value: '#CED6E0' },
+  { name: 'Yellow', value: '#FFD32A' },
+  { name: 'Coral', value: '#FF7F50' },
+  { name: 'Pink', value: '#FC427B' },
+  { name: 'Red', value: '#FF4D4D' },
+  { name: 'Mint', value: '#33D9B2' },
+  { name: 'Green', value: '#05C46B' },
+  { name: 'Blue', value: '#1E90FF' },
+  { name: 'Purple', value: '#921CE8' },
+  { name: 'Black', value: '#2F3542' },
+];
+
 module.exports = appData;
