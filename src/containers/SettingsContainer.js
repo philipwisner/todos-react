59a34d7afb2687bb90c9f456b4ad371f60c737e9
Philip Wisner <philipwisner@gmail.com>
2019-09-20 09:39:09 -0400

    added settings container and functionality

--- src/containers/SettingsContainer.js
+++ src/containers/SettingsContainer.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import '../styles/Containers.scss';
+import appData from '../data';
+import { ColorPicker } from '../components';
 
-const SettingsContainer = () => {
-  return (
-    <div className="About">
-      <h2 className="SecondaryHeader">Settings</h2>
-      <p className="Info">Todo was built using React, Express, Node, MongoDB and Love</p>
-    </div>
-  );
-};
+class SettingsContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      colors: appData.colors,
+      selectedColor: '#921CE8',
+      saved: false,
+    };
+  }
+
+  setThemeColor = color => {
+    this.setState({ selectedColor: color, saved: true }, () => {
+      setTimeout(() => {
+        this.setState({ saved: false });
+      }, 1000);
+    });
+  };
+
+  render() {
+    return (
+      <div className="settings">
+        <h2 className="secondary-header">Settings</h2>
+        <p className="subheader">Color Theme</p>
+        <ColorPicker
+          colors={this.state.colors}
+          selectedColor={this.state.selectedColor}
+          setThemeColor={this.setThemeColor}
+        />
+        {this.state.saved ? <p className="saved">Saved</p> : null}
+      </div>
+    );
+  }
+}
 
 export default SettingsContainer;
