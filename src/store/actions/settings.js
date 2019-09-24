7d1cb0c42fd4df1f641c12c032b139ecbe6f749d
Philip Wisner <philipwisner@gmail.com>
2019-09-24 11:29:26 -0400

    added action

--- src/store/actions/settings.js
+++ src/store/actions/settings.js
@@ -1,8 +1,14 @@
 export const Types = {
   SET_COLOR_THEME: 'SET_COLOR_THEME',
+  SET_DEFAULT_TAB: 'SET_DEFAULT_TAB',
 };
 
 export const updateColorTheme = theme => ({
   type: Types.SET_COLOR_THEME,
   payload: theme,
+});
+
+export const updateDefaultTab = tab => ({
+  type: Types.SET_DEFAULT_TAB,
+  payload: tab,
 });
