9fdb7e358eaba621d03182855089c0a11dc7601f
Philip Wisner <philipwisner@gmail.com>
2019-09-25 10:47:37 -0400

    updated to use state

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ import { Provider } from 'react-redux';
 import { HeaderNav } from './components';
 import { Home, Settings } from './pages';
 import configureStore from './store/store';
-import appData from './data';
 import './styles/App.scss';
 
 export const store = configureStore();
-const isDarkMode = appData.selectedColor === '#CED6E0';
 
 function App() {
+  const themeColor = store.getState().settings.setColorTheme;
+  const isDarkMode = themeColor === '#CED6E0';
   return (
     <Provider store={store}>
       <div className={isDarkMode ? 'app dark' : 'app'}>
         <Router>
           <div className="header-nav">
-            <HeaderNav themeColor={appData.selectedColor} />
+            <HeaderNav themeColor={themeColor} />
           </div>
           <div className="router-outlet">
             <Switch>
