9eb71176a77c4274d5da214f7a37a5720db69895
Philip Wisner <philipwisner@gmail.com>
2019-09-28 20:17:41 -0400

    moved provider and store to index

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { HeaderNav } from './components';
 import { Home, Settings } from './pages';
-import configureStore from './store/store';
 import './styles/App.scss';
 
-export const store = configureStore();
-
 function App() {
-  const themeColor = store.getState().settings.setColorTheme;
+  const themeColor = useSelector(state => state.settings.updateSettings.themeColor);
   const isDarkMode = themeColor === '#CED6E0';
   return (
-    <Provider store={store}>
-      <div className={isDarkMode ? 'app dark' : 'app'}>
-        <Router>
-          <div className="header-nav">
-            <HeaderNav themeColor={themeColor} />
-          </div>
-          <div className="router-outlet">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route path="/settings" component={Settings} />
-            </Switch>
-          </div>
-        </Router>
-      </div>
-    </Provider>
+    <div className={isDarkMode ? 'app dark' : 'app'}>
+      <Router>
+        <div className="header-nav">
+          <HeaderNav themeColor={themeColor} />
+        </div>
+        <div className="router-outlet">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/settings" component={Settings} />
+          </Switch>
+        </div>
+      </Router>
+    </div>
   );
 }
 
